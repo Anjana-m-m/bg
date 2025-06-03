15c9b8a6e21d51c6d621221928034c4fe522df0b
Anjana-m-m <anjanamm06@gmail.com>
2025-06-03 22:02:32 +0530

    Update script.js

--- study/script.js
+++ study/script.js
@@ -3,15 +3,13 @@ document.addEventListener("click", (e) => {
       const cage=document.createElement("div");
       cage.classList.add("cage");
       cage.style.position = "absolute";
-      cage.style.left = `${e.clientX -100}px`;
-      cage.style.top = `${e.clientY -100}px`;
 
       const heading = document.createElement("h2");
       heading.innerText = "OOOPS!";
       heading.style.fontSize = "10px";
       heading.style.textAlign = "center";
       heading.style.color = "#fff";
-      heading.style.padding="20px";
+      heading.style.padding="100px";
 
       cage.appendChild(heading);
       document.body.appendChild(cage);
