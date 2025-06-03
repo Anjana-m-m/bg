3e50e24486986a1e8f64e404178f2f9e4c88b30c
Anjana-m-m <anjanamm06@gmail.com>
2025-06-03 21:46:06 +0530

    Add files via upload

---
+++ study/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON"){
+      const cage=document.createElement("div");
+      cage.classList.add("cage");
+      cage.style.position = "absolute";
+      cage.style.left = `${e.clientX -100}px`;
+      cage.style.top = `${e.clientY -100}px`;
+
+      const heading = document.createElement("h2");
+      heading.innerText = "OOPS!";
+      heading.style.fontSize = "10px";
+      heading.style.textAlign = "center";
+      heading.style.color = "#000";
+      heading.style.padding="20px";
+
+      cage.appendChild(heading);
+      document.body.appendChild(cage);
+
+
+      setTimeout(() => {
+        cage.remove();
+      }, 8000);
+    }
+  else{
+    const butterfly = document.createElement("div");
+    butterfly.classList.add("butterfly");
+    butterfly.style.left = `${e.clientX - 20}px`;
+    butterfly.style.top = `${e.clientY - 20}px`;
+  
+    document.body.appendChild(butterfly);
+  
+  
+    setTimeout(() => {
+      butterfly.remove();
+    }, 2000);
+  }
+});
+  
